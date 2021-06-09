773834b0e6a3158d9632ed1c1df83eefb69d29e9
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-06-09 19:39:19 +0100

    init project, added AccountsPage, Form and PageHeader Components

---
+++ src/Components/AccountsPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import PageHeader from "../Utilities/PageHeader"
+
+const AccountsPage = () => (
+        <PageHeader content="accounts" />
+)
+
+export default AccountsPage
---
+++ src/Components/Utilities/PageHeader/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const PageHeader = (props) => <h1>{ props.content }</h1>
+
+export default PageHeader
